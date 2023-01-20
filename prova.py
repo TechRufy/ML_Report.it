92ff35a71f90bbd3baa0910f7888b6bcbbc91b22
TechRufy <alb.genovese@gmail.com>
2023-01-20 23:10:15 +0100

    inizio modulo

--- prova.py
+++ prova.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.post("http://127.0.0.1:5000", json={"ciaot": "ao"})
+r = requests.post("https://mlreport.azurewebsites.net/", json={"ciaot": "ao"})
 
 print(r.json())
 
