19de7c060c680a59c40a1b2837377efdcd72eb8c
TechRufy <alb.genovese@gmail.com>
2023-03-02 20:29:39 +0100

    prova dbscan

--- ModuloML/main.py
+++ ModuloML/main.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 # importiamo le librerie necessarie da sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 from sklearn.decomposition import IncrementalPCA
 from sklearn import decomposition
@@ -17,11 +18,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("dati.csv", encoding="ISO-8859-1")
-indexNames = df[df['Category'] == 'other_cyberbullying'].index
-df.drop(indexNames, inplace=True)
-indexNames = df[df['Category'] == 'religion'].index
-df.drop(indexNames, inplace=True)
+df = pd.read_csv("dati.csv")
+# indexNames = df[df['Category'] == 'other_cyberbullying'].index
+# df.drop(indexNames, inplace=True)
+# indexNames = df[df['Category'] == 'religion'].index
+# df.drop(indexNames, inplace=True)
 # indexNames = df[df['Category'] == 'age'].index
 # df.drop(indexNames, inplace=True)
 # indexNames = df[df['Category'] == 'ethnicity'].index
@@ -29,16 +30,20 @@ df.drop(indexNames, inplace=True)
 # indexNames = df[df['Category'] == 'gender'].index
 # df.drop(indexNames, inplace=True)
 
-vectorizer = TfidfVectorizer(max_df=50) #stavi a modificare i valori qui, prova
+vectorizer = TfidfVectorizer()  # stavi a modificare i valori qui, prova
 # fit_transform applica il TF-IDF ai testi puliti - salviamo la matrice di vettori in X
-X = vectorizer.fit_transform(df['clean'].values.astype('U'))
-
+X = vectorizer.fit_transform(df['testi puliti'].values.astype('U'))
+print("ciao")
 # inizializziamo il KMeans con 4 cluster
-kmeans = KMeans(n_clusters=3, random_state=20, n_init=10)
+kmeans = KMeans(n_clusters=6, random_state=20, n_init=10)
 kmeans.fit(X)
 clusters = kmeans.labels_
+print("ciao")
+dbmeans = DBSCAN(n_jobs=-1,eps=1)
+dbmeans.fit(X)
+clustersDB = dbmeans.labels_
 
-
+print("ciao")
 def get_top_keywords(n_terms):
     """Questa funzione restituisce le keyword per ogni centroide del KMeans"""
     df = pd.DataFrame(X.todense()).groupby(clusters).mean()  # raggruppa il vettore TF-IDF per gruppo
@@ -51,17 +56,17 @@ def get_top_keywords(n_terms):
 
 
 # inizializziamo la PCA con 2 componenti
-# pca = PCA(n_components=2, random_state=42)
-ipca = IncrementalPCA(n_components=2, batch_size=10)
+ipca = IncrementalPCA(n_components=2, batch_size=50)
+print("ciao")
 # passiamo alla pca il nostro array X
-# pca_vecs = pca.fit_transform(X.toarray())
-
 pca_vecs = ipca.fit_transform(X)
+
 # salviamo le nostre due dimensioni in x0 e x1
 x0 = pca_vecs[:, 0]
 x1 = pca_vecs[:, 1]
 
-df['cluster'] = clusters
+df['clusterKmeans'] = clusters
+df["clusterDBmeans"] = clustersDB
 df['x0'] = x0
 df['x1'] = x1
 
@@ -72,7 +77,7 @@ plt.title("Raggruppamento TF-IDF + KMeans", fontdict={"fontsize": 18})
 plt.xlabel("X0", fontdict={"fontsize": 16})
 plt.ylabel("X1", fontdict={"fontsize": 16})
 # creiamo diagramma a dispersione con seaborn, dove hue è la classe usata per raggruppare i dati
-sns.scatterplot(data=df, x='x0', y='x1', hue='cluster', palette="bright")
+sns.scatterplot(data=df, x='x0', y='x1', hue='clusterKmeans', palette="bright")
 plt.show()
 
 plt.figure(figsize=(9, 9), dpi=200)
@@ -81,10 +86,19 @@ plt.title("Raggruppamento TF-IDF + KMeans", fontdict={"fontsize": 18})
 # settiamo nome assi
 plt.xlabel("X0", fontdict={"fontsize": 16})
 plt.ylabel("X1", fontdict={"fontsize": 16})
-sns.scatterplot(data=df, x='x0', y='x1', hue='Category', palette="bright")
+sns.scatterplot(data=df, x='x0', y='x1', hue='discriminazione', palette="bright")
 plt.show()
 
-f = open("dati.txt", "w")
+plt.figure(figsize=(9, 9), dpi=200)
+# settiamo titolo
+plt.title("Raggruppamento TF-IDF + KMeans", fontdict={"fontsize": 18})
+# settiamo nome assi
+plt.xlabel("X0", fontdict={"fontsize": 16})
+plt.ylabel("X1", fontdict={"fontsize": 16})
+sns.scatterplot(data=df, x='x0', y='x1', hue='clusterDBmeans', palette="bright")
+plt.show()
 
-df.to_string(encoding="utf-8", buf="dati.txt", columns=["clean", "cluster", "Category", "x0", "x1"])
+f = open("postDati.csv", "w", newline="")
+
+df.to_csv(encoding="ISO-8859-1", path_or_buf=f, index=False)
 quit()
--- moduloPreProcessing/PreProcessing.py
+++ moduloPreProcessing/PreProcessing.py
@@ -32,97 +32,105 @@ def preprocess_text(text: str, remove_stopwords=True) -> str:
     Restituisce:
         str: testo pulito
     """
+
+    TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+
+    text = text.lower()
+
+    text = re.sub(TEXT_CLEANING_RE, " ", text)
+
+    listStopWord = stopwords.words("english")
+
+    listStopWord.extend(['rt', 'mkr', 'didn', 'bc', 'n', 'm', 'im', 'll', 'y', 've', 'u', 'ur', 'don', 't', 's'])
+
     # rimuovi link
-    text = re.sub(r"rt\s", " ", text)
-
-    text = re.sub(r"http\S+", " ", text)
-
-    text = re.sub(r"#\S+", " ", text)
-
-    text = re.sub(r"RT\s", " ", text)
-
-    text = re.sub(r"@\S+", " ", text)
-    # rimuovi numeri e caratteri speciali
-    text = re.sub("[^A-Za-z]+", " ", text)
-    # rimuovere le stopword
+    # text = re.sub(r"rt\s", " ", text)
+    #
+    # text = re.sub(r"http\S+", " ", text)
+    #
+    # text = re.sub(r"#\S+", " ", text)
+    #
+    # text = re.sub(r"RT\s", " ", text)
+    #
+    # text = re.sub(r"@\S+", " ", text)
+    # # rimuovi numeri e caratteri speciali
+    # text = re.sub("[^A-Za-z]+", " ", text)
+    # # rimuovere le stopword
     if remove_stopwords:
         # 1. crea token
         tokens = nltk.word_tokenize(text)
         # 2. controlla se è una stopword
-        tokens = [w for w in tokens if not w.lower() in stopwords.words("english")]
+        tokens = [w for w in tokens if not w.lower() in listStopWord]
         # 3. unisci tutti i token
         text = " ".join(tokens)
     # restituisci il testo pulito, senza spazi eccessivi, in minuscolo
 
-    text = re.sub(r"rt\s", " ", text)
-
-    text = text.lower().strip()
-
     return text
 
 
-def process_file(text):
-    r = pd.DataFrame(data={}, columns=["clean", "Category"])
-    l = []
-    for index, rows in text.iterrows():
-        words = preprocess_text(rows["tweet_text"], remove_stopwords=True)
-        l.append((words, rows["cyberbullying_type"]))
+# def process_file(text):
+#     r = pd.DataFrame(data={}, columns=["clean", "Category"])
+#     l = []
+#     for index, rows in text.iterrows():
+#         words = preprocess_text(rows["testi"], remove_stopwords=True)
+#         l.append((words, rows["cyberbullying_type"]))
+#
+#     r = pd.concat([r, pd.DataFrame(data=l, columns=["clean", "Category"])])
+#     # risultati.put(r)
+#     return r
 
-    r = pd.concat([r, pd.DataFrame(data=l, columns=["clean", "Category"])])
-    # risultati.put(r)
-    return r
 
+data = pd.read_csv("cyberbullying_tweets.csv")
 
-df = pd.read_csv("cyberbullying_tweets.csv", encoding="ISO-8859-1")
+data = data.rename(columns={'tweet_text': 'testi', 'cyberbullying_type': 'discriminazione'})
 
-indexNames = df[df['cyberbullying_type'] == 'not_cyberbullying'].index
-# Delete these row indexes from dataFrame
-df.drop(indexNames, inplace=True)
+data["valore_discriminazione"] = data['discriminazione'].replace(
+    {"religion": 1, "age": 2, "ethnicity": 3, "gender": 4, "other_cyberbullying": 5, "not_cyberbullying": 6})
+
+# indexNames = df[df['cyberbullying_type'] == 'not_cyberbullying'].index
+# # Delete these row indexes from dataFrame
+# df.drop(indexNames, inplace=True)
 
 # indexNames = df[df['cyberbullying_type'] == 'other_cyberbullying'].index
 # # Delete these row indexes from dataFrame
 # df.drop(indexNames, inplace=True)
 
 
-start = 0
-threads = []
-results = multiprocessing.Queue()
-n_threads = 1
-size = int((df.size / df.columns.size) / n_threads)
-
-wordnet.ensure_loaded()
-stopwords.ensure_loaded()
-clean = pd.DataFrame(data={}, columns=["clean", "Category"])
-lista = []
-
-for _ in range(n_threads):
-    p = df.iloc[start:(start + size)]
-    start = start + size
-    lista.append(p)
-
+# start = 0
+# threads = []
+# results = multiprocessing.Queue()
+# n_threads = 1
+# size = int((df.size / df.columns.size) / n_threads)
+#
+# wordnet.ensure_loaded()
+# stopwords.ensure_loaded()
+# clean = pd.DataFrame(data={}, columns=["clean", "Category"])
+# lista = []
+#
+# for _ in range(n_threads):
+#     p = df.iloc[start:(start + size)]
+#     start = start + size
+#     lista.append(p)
+#
 pre = time.time()
+#
+# with ThreadPoolExecutor(max_workers=n_threads) as ex:
+#     risultato = ex.map(process_file, lista)
+#     for ris in risultato:
+#         clean = pd.concat([clean, ris])
 
-with ThreadPoolExecutor(max_workers=n_threads) as ex:
-    risultato = ex.map(process_file, lista)
-    # future_to_url = {ex.submit(process_file, p, results): p for p in lista}
-    # for future in as_completed(future_to_url):
-    #     url = future_to_url[future]
-    # try:
-    #     data = future.result()
-    # except Exception as exc:
-    #     print("problemi")
-    # else:
-    #     print(data)
-    for ris in risultato:
-        clean = pd.concat([clean, ris])
+
+data['testi puliti'] = data['testi'].apply(preprocess_text)
+
+del data["testi"]
 
 post = time.time()
 print("tempo {0}".format(post - pre))
 script_dir = os.path.dirname(__file__)[:-20]
 rel_path = "ModuloML/dati.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
-f = open(abs_file_path, "w",newline='')
-clean["clean"].replace('', numpy.nan, inplace=True)
-clean.dropna(inplace=True)
-clean.to_csv(path_or_buf=f, encoding="ISO-8859-1", index=False)
+f = open(abs_file_path, "w", newline='')
+data["testi puliti"].replace('', numpy.nan, inplace=True)
+data.dropna(inplace=True)
+data.to_csv(path_or_buf=f, index=False)
 quit()
