581d8bb349f780bc5135507c2330d8f76b03a479
TechRufy <alb.genovese@gmail.com>
2023-03-05 19:20:51 +0100

    rimozione sav

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask import request
+import pickle
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return request.get_data()
+    loaded_model = pickle.load(open("RandomForest.sav", 'rb'))
+    testo = request.get_data()
+    result = loaded_model.predict(testo)
+    print(result)
+
+    return result
